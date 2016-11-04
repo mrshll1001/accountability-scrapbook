a99b300740fcf5cc689711b814a0336d6a72cfc2
Matt Marshall <m.marshall@newcastle.ac.uk>
2016-11-04 12:54:08 +0000

    Vector asset and new activity

---
+++ app/src/main/java/uk/mrshll/matt/accountabilityscrapbook/AddSpendscrapActivity.java
@@ -0,0 +1,15 @@
+package uk.mrshll.matt.accountabilityscrapbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSpendscrapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_spendscrap);
+
+        
+    }
+}
