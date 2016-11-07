15375ae2e5bbe4f2dcb0807f449ae36697c622b9
Matt Marshall <m.marshall@newcastle.ac.uk>
2016-11-07 14:08:56 +0000

    Began searching for activity, think I need an API key

--- app/src/main/java/uk/mrshll/matt/accountabilityscrapbook/AddEventscrapActivity.java
+++ app/src/main/java/uk/mrshll/matt/accountabilityscrapbook/AddEventscrapActivity.java
@@ -1,13 +1,52 @@
 package uk.mrshll.matt.accountabilityscrapbook;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 public class AddEventscrapActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_eventscrap);
+        final Activity mug = this;
+
+        final ImageButton getLocation = (ImageButton) findViewById(R.id.create_eventscrap_mapbutton);
+        getLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                try {
+                    int PLACE_PICKER_REQUEST = 3;
+                    PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                    startActivityForResult(builder.build(mug), PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesNotAvailableException e)
+                {
+
+                } catch (GooglePlayServicesRepairableException e)
+                {
+
+                }
+
+
+
+            }
+        });
+
+
+
     }
 }
