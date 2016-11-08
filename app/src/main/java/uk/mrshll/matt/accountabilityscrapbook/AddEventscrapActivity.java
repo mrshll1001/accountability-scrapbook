67c1253881eb28ec946d67c132ff93152bd4ece4
Matt Marshall <m.marshall@newcastle.ac.uk>
2016-11-08 14:44:44 +0000

    Event interface done

--- app/src/main/java/uk/mrshll/matt/accountabilityscrapbook/AddEventscrapActivity.java
+++ app/src/main/java/uk/mrshll/matt/accountabilityscrapbook/AddEventscrapActivity.java
@@ -2,22 +2,36 @@ package uk.mrshll.matt.accountabilityscrapbook;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class AddEventscrapActivity extends AppCompatActivity {
 
-    String thePlace;
+    String placeName;
+    String placeAddress;
+    LatLng placeLatLong;
     int PLACE_PICKER_REQUEST = 3;
 
     @Override
@@ -26,7 +40,11 @@ public class AddEventscrapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_eventscrap);
 
-        this.thePlace = "";
+        // Set default values for the place
+        this.placeName = null;
+        this.placeAddress = null;
+        this.placeLatLong = null;
+
 
         final Activity mug = this;
 
@@ -62,9 +80,60 @@ public class AddEventscrapActivity extends AppCompatActivity {
         {
             if(resultCode == RESULT_OK)
             {
-                Place place = PlacePicker.getPlace(data, this);
-                String toast = String.format("You have chosen: %s", place.getAddress());
-                Toast.makeText(this, toast, Toast.LENGTH_SHORT).show();
+                Place place = PlacePicker.getPlace(this, data);
+                String toast = String.format("Hello: %s", place.getLatLng());
+                Toast.makeText(this, toast, Toast.LENGTH_LONG).show();
+
+                this.placeAddress = place.getAddress().toString();
+                this.placeLatLong = place.getLatLng();
+                this.placeName = place.getName().toString();
+
+                // Update the UI
+                final TextView address = (TextView) findViewById(R.id.create_eventscrap_address);
+                address.setText(this.placeName + ", " + this.placeAddress);
+
+                // Try to fetch a static map from GMaps and set the image as the background
+                AsyncTask<Void, Void, Bitmap> getMapImage = new AsyncTask<Void, Void, Bitmap>() {
+                    @Override
+                    protected Bitmap doInBackground(Void... voids)
+                    {
+                        Bitmap map = null;
+                        try
+                        {
+
+                            URL url = new URL("http://maps.google.com/maps/api/staticmap?center="+placeLatLong.latitude+","+placeLatLong.longitude+"&zoom=15&size=200x200&sensor=false");
+                            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                            connection.setRequestMethod("GET");
+                            InputStream stream = connection.getInputStream();
+                            map = BitmapFactory.decodeStream(stream);
+
+
+                        } catch (MalformedURLException e)
+                        {
+                            Toast.makeText(AddEventscrapActivity.this, "Malformed URL", Toast.LENGTH_SHORT).show();
+                        } catch (IOException e)
+                        {
+                            Toast.makeText(AddEventscrapActivity.this, "IO Exception", Toast.LENGTH_SHORT).show();
+                        }
+
+                        return map;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Bitmap bitmap) {
+                        if (bitmap != null)
+                        {
+                            final ImageView well = (ImageView) findViewById(R.id.create_eventcrap_imagewell);
+                            well.setImageBitmap(bitmap);
+                            address.setTextColor(getResources().getColor(R.color.colorWellGrey));
+
+                        }
+                    }
+                };
+
+                getMapImage.execute();
+
+
 
             }
         }
