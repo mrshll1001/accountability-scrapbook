23a1f7a8b2b438e9ea5657c17a983be62b835fb7
Matt Marshall <m.marshall@newcastle.ac.uk>
2016-11-18 15:28:21 +0000

    Combined all scrap types into single model

---
+++ app/src/main/java/uk/mrshll/matt/accountabilityscrapbook/model/Scrap.java
@@ -0,0 +1,143 @@
+package uk.mrshll.matt.accountabilityscrapbook.model;
+
+import java.util.Date;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+
+/**
+ * Generic Scrap class to replace all of the distinct object types.
+ * This allows everything to be collated, and allows the data structure flexibility to
+ * encapsulate more contexts, together, in the future.
+ *
+ * Created by marshall on 18/11/16.
+ */
+
+public class Scrap extends RealmObject
+{
+    // Type definitions
+    static int TYPE_SPEND = 0;
+    static int TYPE_QUOTE = 1;
+    static int TYPE_EVENT = 2;
+    static int TYPE_PHOTO = 4;
+
+    // Meta. Type and date
+    private Date dateCreated;               // Date created on system
+    private Date dateGiven;                 // Date given by the user, so that they can retrotag it
+    private int type;                       // Type, used for interface stuff.
+
+    // Lists of tags
+    private RealmList<Tag> inheritedTags;   // Tags inherited from parent scrapbooks
+    private RealmList<Tag> customTags;      // Tags individual to the Scrap
+
+    // Independent fields
+    private String name;                    // Name of the item
+
+    private double spendValue;              // If item is a spend type, this is its value
+
+    private String quoteText;               // If item is a quote, this is its content
+    private String quoteSource;             // If item is a quote, this its speaker/origin
+
+    private String placeLatLng;             // If item is an event, this is the address
+    private String placeName;               // If item is an event, this is the address
+    private String placeAddress;            // If item is an event, this is the address
+
+    private String photoUri;                // If item is a photo, this is the uro of the file
+
+
+    public RealmList<Tag> getInheritedTags() {
+        return inheritedTags;
+    }
+
+    public RealmList<Tag> getCustomTags() {
+        return customTags;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getDateGiven() {
+        return dateGiven;
+    }
+
+    public void setDateGiven(Date dateGiven) {
+        this.dateGiven = dateGiven;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpendValue() {
+        return spendValue;
+    }
+
+    public void setSpendValue(double spendValue) {
+        this.spendValue = spendValue;
+    }
+
+    public String getQuoteText() {
+        return quoteText;
+    }
+
+    public void setQuoteText(String quoteText) {
+        this.quoteText = quoteText;
+    }
+
+    public String getQuoteSource() {
+        return quoteSource;
+    }
+
+    public void setQuoteSource(String quoteSource) {
+        this.quoteSource = quoteSource;
+    }
+
+    public String getPlaceLatLng() {
+        return placeLatLng;
+    }
+
+    public void setPlaceLatLng(String placeLatLng) {
+        this.placeLatLng = placeLatLng;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public String getPhotoUri() {
+        return photoUri;
+    }
+
+    public void setPhotoUri(String photoUri) {
+        this.photoUri = photoUri;
+    }
+
+    public String getPlaceAddress() {
+        return placeAddress;
+    }
+
+    public void setPlaceAddress(String placeAddress) {
+        this.placeAddress = placeAddress;
+    }
+}
