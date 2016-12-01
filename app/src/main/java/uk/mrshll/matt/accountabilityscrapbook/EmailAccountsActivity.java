87bef41ada5d9b697756e23e7ab45ab611373a60
Matt Marshall <m.marshall@newcastle.ac.uk>
2016-12-01 09:45:46 +0000

    Installed Open CSV. Ballache

--- app/src/main/java/uk/mrshll/matt/accountabilityscrapbook/EmailAccountsActivity.java
+++ app/src/main/java/uk/mrshll/matt/accountabilityscrapbook/EmailAccountsActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.opencsv.CSVWriter;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -57,7 +59,7 @@ public class EmailAccountsActivity extends AppCompatActivity
                 // Create a new CSV file
                 File budgetFile = new File(folder.getPath() + System.currentTimeMillis());
                 try {
-                    // TODO install the csvwriter library from apache!! https://sourceforge.net/projects/opencsv/
+
                     CSVWriter writer = new CSVWriter(new FileWriter(budgetFile));
 
                 } catch (IOException e) {
