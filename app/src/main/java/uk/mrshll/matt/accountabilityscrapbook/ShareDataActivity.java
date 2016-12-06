dfa4d2b94640021e625c3c8de8b077d4feebb5e7
Matt Marshall <m.marshall@newcastle.ac.uk>
2016-12-06 15:00:32 +0000

    Began sharing activity

---
+++ app/src/main/java/uk/mrshll/matt/accountabilityscrapbook/ShareDataActivity.java
@@ -0,0 +1,14 @@
+package uk.mrshll.matt.accountabilityscrapbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShareDataActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_data);
+    }
+}
